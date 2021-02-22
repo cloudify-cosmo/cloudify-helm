5359dd6c005ee582426ea0c2b1bc8289e3c9c9f7
Igor Zhivilo <igorzhivilo@Igors-MacBook-Pro.local>
2021-02-22 14:22:01 +0200

    Update supervisord configuration to fix bug related to latest images

--- reinstall.sh
+++ reinstall.sh
@@ -8,6 +8,6 @@ kubectl delete pvc cloudify-data-cloudify-manager-worker-0
 sleep 10
 helm install rabbitmq bitnami/rabbitmq -f ./cloudify-manager-worker/external/rabbitmq-values.yaml -n cfy-helm
 helm install postgres bitnami/postgresql -f ./cloudify-manager-worker/external/postgres-values.yaml -n cfy-helm
-sleep 20
+sleep 30
 helm install cloudify-manager-worker ./cloudify-manager-worker -f cloudify-manager-worker/values.yaml
 
