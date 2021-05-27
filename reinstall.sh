d42e3e063cbe35e05864e1834762f6345a63298c
Igor Zhivilo <igorzhivilo@Igors-MacBook-Pro.local>
2021-05-27 14:53:17 +0300

    RD-2442 use efs-provisoner with EFS PVC

--- reinstall.sh
+++ reinstall.sh
@@ -8,6 +8,6 @@ kubectl delete pvc cloudify-data-cloudify-manager-worker-0
 sleep 10
 helm install rabbitmq bitnami/rabbitmq -f ./cloudify-manager-worker/external/rabbitmq-values.yaml
 helm install postgres bitnami/postgresql -f ./cloudify-manager-worker/external/postgres-values.yaml
-sleep 30
+sleep 60
 helm install cloudify-manager-worker ./cloudify-manager-worker -f cloudify-manager-worker/values.yaml
 
