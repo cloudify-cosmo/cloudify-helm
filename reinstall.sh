6837f79bebc250934a24339fe7dbf1db2102f753
Igor Zhivilo <igorzhivilo@Igors-MacBook-Pro.local>
2021-06-08 11:58:54 +0300

    RD-249 define replica num in values.yaml

--- reinstall.sh
+++ reinstall.sh
@@ -4,10 +4,8 @@ helm uninstall rabbitmq
 helm uninstall postgres
 helm uninstall cloudify-manager-worker
 sleep 10
-kubectl delete pvc cloudify-data-cloudify-manager-worker-0
-sleep 10
 helm install rabbitmq bitnami/rabbitmq -f ./cloudify-manager-worker/external/rabbitmq-values.yaml
 helm install postgres bitnami/postgresql -f ./cloudify-manager-worker/external/postgres-values.yaml
-sleep 30
+sleep 40
 helm install cloudify-manager-worker ./cloudify-manager-worker -f cloudify-manager-worker/values.yaml
 
