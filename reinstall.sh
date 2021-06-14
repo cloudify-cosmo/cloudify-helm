152e13a48a81408e8c53d6ee6f25744a9250b4d0
Igor Zhivilo <igorzhivilo@Igors-MacBook-Pro.local>
2021-06-14 11:11:16 +0300

    RD-2491 update success threshold to 3 in readiness probe

--- reinstall.sh
+++ reinstall.sh
@@ -6,6 +6,6 @@ helm uninstall cloudify-manager-worker
 sleep 10
 helm install rabbitmq bitnami/rabbitmq -f ./cloudify-manager-worker/external/rabbitmq-values.yaml
 helm install postgres bitnami/postgresql -f ./cloudify-manager-worker/external/postgres-values.yaml
-sleep 40
+sleep 60
 helm install cloudify-manager-worker ./cloudify-manager-worker -f cloudify-manager-worker/values.yaml
 
