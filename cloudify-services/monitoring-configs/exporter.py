949401ceb0188bad04ab77bf9f857b7e9f117565
Łukasz Maksymczuk <tehasdf@users.noreply.github.com>
2023-03-27 09:43:48 +0200

    RND-180 "Pod status" prometheus exporter (#87)
    * implement a server exporting the pod statuses
    
    this is a simple .py HTTP server that on any GET, returns
    the status of all pods in the current namespace in the prometheus
    metrics format, only giving up{} for each.
    
    This can be a target for prometheus scraping, when we only want
    to know if pods are up or not.
    
    It has no dependencies other than the stdlib, so we can run it on
    any py3.8+ image.
    
    * chart: update prometheus with the new exporter
    
    - add the new exporter container
    - use a configmap for the exporter source
    - declare the serviceaccount etc
    
    * add prometheus config yaml
    
    it's now a separate file, not inline in the chart.
    This config defines scraping from the local cloudify "exporter".
    
    We set honor_labels=true, so that the job/instance labels are as
    reported from the exporter, not "cloudify" (job_name)

---
+++ cloudify-services/monitoring-configs/exporter.py
@@ -0,0 +1,70 @@
+"""exporter.py: a script that re-exposes pod statuses in a prometheus format
+
+This runs a HTTP server on 127.0.0.1:8000 that responds to any GET with
+a text response containing the up{} metric for every pod found in the local
+k8s namespace.
+"""
+import json
+import os
+import pathlib
+import ssl
+import http.server
+import urllib.request
+
+
+def get_metrics():
+    """Return metrics based on the pods response from the k8s api.
+
+    Fetch the list of pods in the current namespace, and return them in the
+    format of prometheus.
+    For example, this will return a list like:
+    ['up{job="rest-service", instance="192.168.2.2"}']
+    """
+    sa_dir = pathlib.Path('/var/run/secrets/kubernetes.io/serviceaccount')
+    k8s_addr = os.environ['KUBERNETES_SERVICE_HOST']
+    namespace = (sa_dir / 'namespace').read_text()
+    token = (sa_dir / 'token').read_text()
+
+    url = f'https://{k8s_addr}/api/v1/namespaces/{namespace}/pods'
+    sslcontext = ssl.create_default_context()
+    sslcontext.load_verify_locations(cafile=sa_dir / 'ca.crt')
+
+    with urllib.request.urlopen(
+        urllib.request.Request(
+            url,
+            method='GET',
+            headers={'Authorization': f'Bearer {token}'},
+        ),
+        context=sslcontext,
+    ) as req:
+        pods = json.load(req)
+
+    parts = []
+    for pod in pods['items']:
+        try:
+            app = pod['metadata']['labels']['app']
+            pod_ip = pod['status']['podIP']
+            is_up = all(
+                cs['ready'] for cs in pod['status']['containerStatuses']
+            )
+        except KeyError:
+            continue
+        parts.append(
+            f'up{{job="{app}", instance="{pod_ip}"}} {1 if is_up else 0}'
+        )
+    return '\n'.join(parts).encode()
+
+
+class MetricsHandler(http.server.BaseHTTPRequestHandler):
+    """A Handler that always returns 200 with the result of get_metrics()"""
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(get_metrics() + b'\n')
+
+
+if __name__ == '__main__':
+    server_address = ('127.0.0.1', 8000)
+    with http.server.HTTPServer(server_address, MetricsHandler) as httpd:
+        httpd.serve_forever()
